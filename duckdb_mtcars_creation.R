4541fd2326738038896628e192a884f09d1b0b85
mechtrix <weber.tim1985@gmail.com>
2024-10-05 14:34:59 +0200

    update duckdb

--- duckdb_mtcars_creation.R
+++ duckdb_mtcars_creation.R
@@ -8,8 +8,8 @@ duckdb_con <- dbConnect(duckdb())
 
 con <- dbConnect(duckdb(), dbdir = "raw_data/mtcars.duckdb", read_only = FALSE)
 
-dbWriteTable(duckdb_con, "mtcars_table", mtcars)
+dbWriteTable(con, "mtcars_table", mtcars)
 
-out <- tbl(duckdb_con,"mtcars_table") |> collect()
+out <- tbl(con,"mtcars_table") |> collect()
 
-dbDisconnect(duckdb_con)
+dbDisconnect(con)
