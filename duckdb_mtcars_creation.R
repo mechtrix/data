021a5b2f32165a88c2046db36d7a828819516950
mechtrix <weber.tim1985@gmail.com>
2024-10-05 14:14:39 +0200

    duckdb upload

---
+++ duckdb_mtcars_creation.R
@@ -0,0 +1,13 @@
+library(duckdb)
+library(dbplyr)
+library(dplyr)
+
+data("mtcars")
+
+duckdb_con <- dbConnect(duckdb())
+
+con <- dbConnect(duckdb(), dbdir = "raw_data/mtcars.duckdb", read_only = FALSE)
+
+dbWriteTable(duckdb_con, "mtcars_table", mtcars)
+
+out <- tbl(duckdb_con,"mtcars_table") |> collect()
