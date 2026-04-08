b787db0d8158273293ee3ad440d3493ea87e74a9
mechtrix <weber.tim1985@gmail.com>
2026-04-08 10:22:43 +0200

    added fim ss26 data

---
+++ raw_data/fim_ss26.R
@@ -0,0 +1,17 @@
+fim_raw <- data.frame(
+  trans = seq(1,12),
+  items = c(
+    c("Cafe, Cereal, Joghurt"), #1
+    c("Cafe, Cereal, Towels"), #2
+    c("Cafe, Cereal, Cookies, Towels"), #3
+    c("Joghurt, Cookies, Cereal"), #4
+    c("Cafe, Joghurt, Cookies"), #5
+    c("Cafe, Cereal, Towels"), #6
+    c("Cafe, Cereal, Towels"), #7
+    c("Joghurt, Towels, Cookies"), #8
+    c("Joghurt, Ceral, Towels"), #9
+    c("Joghurt, Cafe, Cereal"), #10
+    c("Joghurt"), #11
+    c("Joghurt, Cereal, Cookies, Towels") #12
+  )
+)
